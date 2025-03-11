7add5383c0c303a619f671f2547a5568a9563875
MatiasTorterolo <mtorterolo.a@gmail.com>
2025-03-11 16:10:52 -0300

    pequeño cambio en el footer

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class='custom-background-color-footer'>
+<footer class='mt-auto custom-background-color-footer'>
     <div class='container d-flex justify-content-center'>
         <p class='p-3 custom-color-font-footer mb-0'>
             Desarrollado por Matias Torterolo Ambrosini &copy; 2025
